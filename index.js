eb1fe1383837fa2c01d0d5d03ac5048fc32bf96a
jsm4r1 <mariam.ozuna26@compscihigh.org>
2025-09-16 18:54:32 +0000

    create,add,update song

--- index.js
+++ index.js
@@ -20,13 +20,17 @@ console.log(playlist)
 // ----------------------------------
 // OBJECTS SECTION
 // Task 5: Create an object called 'song' with 'title', 'artist', and 'duration' properties
-
+let song = {
+    title: "",
+    artist: "",
+    duration: 
+};
 
 // Task 6: Add a property 'listenedTo' to the 'song' object and set it to false
-
+song.listenedTo = false
 
 // Task 7: Delete the 'artist' property
-
+delete song.artist
 
 // Commit your changes with an appropriate message.
 // ----------------------------------
